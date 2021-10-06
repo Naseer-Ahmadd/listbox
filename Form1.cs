0887850eda27d0ed5bac3a0db108c0e7582c4ba2
Naseer-Ahmadd <naseerrather58@gmail.com>
2021-10-06 15:09:54 +0530

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace listbox
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string t1 = textBox1.Text;
+            string t2 = textBox2.Text;
+            listBox1.Items.Add(t1);
+            listBox2.Items.Add(t2);
+            textBox1.Text = " ";
+            textBox2.Text = " ";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+
+        {
+            if (listBox1.Items.Count > 0)
+            {
+                listBox2.Items.Add(listBox1.SelectedItem);
+                listBox1.Items.Remove(listBox1.SelectedItem);
+            }
+                
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (listBox2.Items.Count > 0)
+            {
+                listBox1.Items.Add(listBox2.SelectedItem);
+                listBox2.Items.Remove(listBox2.SelectedItem);
+            }
+
+            
+        }
+    }
+}
